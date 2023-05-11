d4daa3ae200ada742d692704e02db53d7617b1e4
nguyenkhacnhut <117978018+nguyenkhacnhut@users.noreply.github.com>
2023-05-11 12:00:14 +0700

    search

--- app/src/main/java/com/example/android_20/LessonActivity.java
+++ app/src/main/java/com/example/android_20/LessonActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,11 +13,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.android_20.Lesson.LessonDetailActivity;
@@ -30,8 +42,6 @@ public class LessonActivity extends AppCompatActivity implements LessonAdapter.L
     ArrayList<Lesson> lstLesson;
     LessonAdapter lessonAdapter;
     ArcheryDB archeryDB;
-    ArrayList<Lesson> searchList;
-
     Toolbar tb;
     int Grade, Subject;
     SharedPreferences sharedPreferences;
@@ -94,79 +104,42 @@ public class LessonActivity extends AppCompatActivity implements LessonAdapter.L
         lessonAdapter.notifyDataSetChanged();
     }
 
+//    @Override
+//    protected void onPause() {
+//        resetData();
+//        super.onPause();
+//    }
+
     @Override
     protected void onResume() {
         resetData();
         super.onResume();
     }
 
+//    @Override
+//    protected void onStop() {
+//        resetData();
+//        super.onStop();
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //search view
-        //lay menu ra
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search_view, menu);
-//        menu.getItem(0).setIcon(R.drawable.baseline_search_24);
+      getMenuInflater().inflate(R.menu.search_view, menu);
+      SearchView searchView = (SearchView) menu.findItem(R.id.mnSearch).getActionView();
+      searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+          @Override
+          public boolean onQueryTextSubmit(String query) {
+              Toast.makeText(LessonActivity.this, query, Toast.LENGTH_SHORT).show();
+              return false;
+          }
 
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);//truy cap den dich vu tim kiem cua he thong
-        MenuItem menuItem = menu.findItem(R.id.mnSearch);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));//lay info tim kiem, lien ket doi tuong searchview voi thanh phan tim kiem cua he thong
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(LessonActivity.this);
-        rvListC.setLayoutManager(layoutManager);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {/// Xử lý khi người dùng nhấn nút tìm kiếm trên bàn phím
-                searchList = new ArrayList<>();
-                if(query.length()>0){
-                    for (int i = 0; i < lstLesson.size(); i++){
-                        if(lstLesson.get(i).getName().toUpperCase().contains(query.toUpperCase())){
-                           Lesson lesson = new Lesson();
-                           lesson.setName(lstLesson.get(i).getName());
-                           lesson.setIDLesson(lstLesson.get(i).getIDLesson());
-                           lesson.setUnit(lstLesson.get(i).getUnit());
-                           searchList.add(lesson);
-                        }
-                    }
-                }
-                lstLesson.clear();
-                lstLesson.addAll(searchList);
-                lessonAdapter.notifyDataSetChanged();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {// Xử lý khi người dùng nhập văn bản vào thanh tìm kiếm
-                searchList = new ArrayList<>();
-                if(newText.length()>0){
-                    for (int i = 0; i < lstLesson.size(); i++){
-                        if(lstLesson.get(i).getName().toUpperCase().contains(newText.toUpperCase())){
-
-                            Lesson lesson = new Lesson();
-                            lesson.setName(lstLesson.get(i).getName());
-                            lesson.setIDLesson(lstLesson.get(i).getIDLesson());
-                            lesson.setUnit(lstLesson.get(i).getUnit());
-                            searchList.add(lesson);
-                        }
-                    }
-                    lstLesson.clear();
-                    lstLesson.addAll(searchList);
-                    lessonAdapter.notifyDataSetChanged();
-
-                }
-                else {
-                    lstLesson.clear();
-                    lstLesson.addAll(searchList);
-                    lessonAdapter.notifyDataSetChanged();
-
-                }
-                return false;
-            }
-        });
+          @Override
+          public boolean onQueryTextChange(String newText) {
+              lessonAdapter.getFilter().filter(newText);
+              //Log.d("AAAA", newText);
+              return false;
+          }
+      });
             return super.onCreateOptionsMenu(menu);
     }
 
@@ -184,7 +157,7 @@ public class LessonActivity extends AppCompatActivity implements LessonAdapter.L
                             if(lesson1.getUnit() > lesson2.getUnit()){
                                 return 1;
                             }else {
-                                if(lesson1.getUnit()==lesson2.getIDLesson()){
+                                if(lesson1.getIDLesson()==lesson2.getIDLesson()){
                                     return 0;
                                 }else return -1;
                             }
@@ -193,7 +166,7 @@ public class LessonActivity extends AppCompatActivity implements LessonAdapter.L
                     lessonAdapter.notifyDataSetChanged();
                     Toast.makeText(LessonActivity.this, "Sắp xếp thành công", Toast.LENGTH_SHORT).show();
                     break;
-                case R.id.mnSortName:
+                case  R.id.mnSortName:
                     Collections.sort(lstLesson, new Comparator<Lesson>() {
                         @Override
                         public int compare(Lesson lesson1, Lesson lesson2) {
